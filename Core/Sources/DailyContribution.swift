34e13c57b8e5e8cf222c78424438e4132c2f26c4
박제형 <qkrwpgud1121@gmail.com>
2026-02-05 18:02:10 +0900

    feat(Core): Create DailyContribution Model

---
+++ Core/Sources/DailyContribution.swift
@@ -0,0 +1,22 @@
+//
+//  DailyContribution.swift
+//  Core
+//
+//  Created by 박제형 on 2/5/26.
+//
+
+import Foundation
+
+public struct DailyContribution: Identifiable {
+    public let id: UUID
+    public let date: Date
+    public let totalCount: Int
+    public let completedCount: Int
+    
+    public init(id: UUID = UUID(), date: Date, totalCount: Int, completedCount: Int) {
+        self.id = id
+        self.date = date
+        self.totalCount = totalCount
+        self.completedCount = completedCount
+    }
+}
--- Feature/Sources/DashboardView.swift
+++ Feature/Sources/DashboardView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Core
 
 public struct DashboardView: View {
     

3c425eb36a3aa1c167c44e4ee0b30f5d8bf15014
박제형 <qkrwpgud1121@gmail.com>
2026-02-05 16:59:31 +0900

    feat: Create DashboardView

---
+++ Feature/Sources/DashboardView.swift
@@ -0,0 +1,21 @@
+//
+//  DashboardView.swift
+//  Core
+//
+//  Created by 박제형 on 2/5/26.
+//
+
+import SwiftUI
+
+public struct DashboardView: View {
+    
+    public init() {}
+    
+    public var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DashboardView()
+}
